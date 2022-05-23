beeea3dd5b18e9c50712c59f24e574c75903359a
Nika Perfilova <ees2009.2014@gmail.com>
2022-05-23 20:03:02 +0700

    refactor: use Promise.all

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,27 @@
-const fs = require('fs/promises');
 const path = require('path');
+const fs = require('fs/promises');
 
 const FOLDER_NAME = 'secret-folder';
-const FOLDER_PATH = path.resolve(__dirname, FOLDER_NAME);
 
-(async () => {
-  try {
-    const objects = await fs.readdir(FOLDER_PATH, { withFileTypes: true });
+const printFilesStats = async () => {
+  const folderPath = path.resolve(__dirname, FOLDER_NAME);
 
-    const files = objects.filter((obj) => obj.isFile());
+  const objects = await fs.readdir(folderPath, { withFileTypes: true });
 
-    for (const file of files) {
-      const ext = path.extname(file.name);
-      const name = path.basename(file.name, ext);
-      const stats = await fs.stat(path.resolve(FOLDER_PATH, file.name));
-      console.log(`${name} - ${ext.slice(1)} - ${stats.size}b`);
-    }
-  } catch (e) {
-    console.log(e);
-  }
-})();
+  const files = objects.filter((obj) => obj.isFile());
+
+  const filesStats = files.map((file) => {
+    const { ext, name } = path.parse(file.name);
+    const filePath = path.resolve(folderPath, file.name);
+    const stats = fs.stat(filePath);
+    return Promise.all([name, ext, stats]);
+  });
+
+  Promise.all(filesStats).then((responses) =>
+    responses.forEach(([name, ext, stats]) =>
+      console.log(`${name} - ${ext.slice(1)} - ${stats.size}b`)
+    )
+  );
+};
+
+printFilesStats();
